61b39812533e0fec7aafec803b05ae001d3d2ef6
Daniel Rozenzaft <drozenzaft@stuy.edu>
2016-12-09 06:08:22 -0500

    Minor change of public to private method

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -18,7 +18,7 @@ public class Barcode implements Comparable<Barcode>{
       }
   }
 
-  public static boolean numberString(String s) {
+  private static boolean numberString(String s) {
       for (int i = 0; i < s.length(); i++) {
 	  if (s.charAt(i) < 48 || s.charAt(i) > 57) {
 	      return false;
