2f03e41c29a6b7060b6d34114402572cef20a034
Daniel Rozenzaft <drozenzaft@stuy.edu>
2016-12-15 23:03:58 -0500

    Added runtime, but BubbleSort is still faster than SelectionSort for some reason...

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -4,15 +4,15 @@ public class Sorts {
     public static String name() {
 	return "10.Rozenzaft.Daniel";
     }
-    public static boolean swap(int[] array,int a, int b) {
+    public static void swap(int[] array,int a, int b) {
 	int temp = array[a];
 	array[a] = array[b];
 	array[b] = temp;
-	return true;
     }
     public static void SelectionSort(int[] data) {
 	int temp;
-	for (int a = 0; a < data.length-1; a++) {
+	double time = System.currentTimeMillis();
+     	for (int a = 0; a < data.length-1; a++) {
 	    for (int i = a+1; i < data.length; i++) {
 		if (data[i] < data[a]) {
 		    temp = data[i];
@@ -20,6 +20,8 @@ public class Sorts {
 		}
 	    }
 	}
+	double deltaT = (System.currentTimeMillis() - time)/1000.0;
+	System.out.println("SelectionSort Time: " + deltaT + " seconds");
     }
     public static void insert(int[] data, int insertThis, int putHere) {
 	if (insertThis < putHere) {
@@ -34,25 +36,31 @@ public class Sorts {
 	}
     }
     public static void InsertionSort(int[] data) {
-	//System.out.println(0+","+Arrays.toString(data));
+	double time = System.currentTimeMillis();
 	int j;
 	for (int i = 1; i < data.length; i++) {
-	    j = i-1;
-	    while (j > 0 && data[i] < data[j]) {
-		j--;
+	    j=i-1;
+	    if (data[i] < data[i-1]) {
+		while (j >= 0 && data[i] < data[j]) {
+		    j--;
+		}
+		if (data[i] < data[j+1]) {
+		    if (j >= 0) {
+						 insert(data,i,j+1);
+		    }
+		    else {
+			insert(data,i,0);
+		    }
+		}
 	    }
-	    if (j > 0) {
-		j++;
-	    }
-	    if (j > 0 || data[i] < data[j] && (j != i-1 || i == 1)) {
-		insert(data,i,j);
-	    }
-	    //System.out.println(i+","+data[i]+","+data[j]);
-	    //System.out.println(i+","+j+","+Arrays.toString(data));
 	}
+	//System.out.println(i + ": " +Arrays.toString(data));
+	double deltaT = (System.currentTimeMillis() - time)/1000.0;
+	System.out.println("InsertionSort Time: " + deltaT + " seconds");
     }
     public static void BubbleSort(int[] data) {
 	int i = 0;
+	double time = System.currentTimeMillis();
 	boolean swapped = true;
 	while (i < data.length-1 && swapped) {
 	    swapped = false;
@@ -64,37 +72,30 @@ public class Sorts {
 	    }
 	    i++;
 	}
+	double deltaT = (System.currentTimeMillis() - time)/1000.0;
+    System.out.println("BubbleSort Time: " + deltaT + " seconds");
     }
     public static void main(String[] args) {
-	double[] zStore = new double[10000];
-	double[] z = new double[10000];
-	double num;
-	for (int i = 0; i < 10000; i++) {
-	    num = Math.random() * 10000;
-	    zStore[i] = num;
+	int[] z = new int[80000];
+	int[] y = new int[80000];
+	int[] x = new int[80000];
+	int num;
+      	for (int i = 0; i < z.length; i++) {
+	    num = (int)(Math.random() * 80000);
+	    // if (i < 20) {
+	    //		System.out.println(num);
+	    //}
+	    x[i] = num;
+	    y[i] = num;
 	    z[i] = num;
 	}
-	int[] y = SelectionSort(z);
-	/*int[] a = {6,3,1,7,5,9,4,0,2};
-	//insert(a,3,7);
-	bubbleSort(a);
-	System.out.println(Arrays.toString(a));
-	//insert(a,7,3);
-	//System.out.println(Arrays.toString(a));
-	//InsertionSort(a);
-	//System.out.println(Arrays.toString(a));
-	int[] b = {};
-	//InsertionSort(b);
-	bubbleSort(b);
-	System.out.println(Arrays.toString(b));
-	int[] c = {10};
-	//InsertionSort(c);
-	bubbleSort(c);
-	System.out.println(Arrays.toString(c));
-	int[] d = {400,303,1500,1759,1618,13,11,1436,2168,1172,5280,2016,38387};
-	//InsertionSort(d);
-	bubbleSort(d);
-	System.out.println(Arrays.toString(d));*/
+	SelectionSort(z);
+	//System.out.println(Arrays.toString(Arrays.copyOfRange(z,0,100)));
+	InsertionSort(y);
+	//System.out.println(Arrays.toString(Arrays.copyOfRange(y,0,100)));
+	BubbleSort(x);
+	//System.out.println(Arrays.toString(Arrays.copyOfRange(x,0,100)));
+	System.out.println(Arrays.equals(z,y)&&Arrays.equals(z,x)); 
     }
 }
 	  
