98fb29ae37342333776e0c5b189b45bfa9aba9f0
Daniel Rozenzaft <drozenzaft@stuy.edu>
2016-12-21 20:24:04 -0500

    Completed Temperature GUI!

--- temperature/Window2.java
+++ temperature/Window2.java
@@ -6,35 +6,44 @@ public class Window2 extends JFrame implements ActionListener {
 
  private JButton CtoF,FtoC;
  private JLabel l;
- private JTextField t;
- private JCheckBox c;
- 
+ private JTextField t,u;
+  
   //CONSTRUCTOR SETS EVERYTHING UP
   public Window2() {
      this.setTitle("Temperature Conversion!");
      this.setSize(600,400);
      this.setLocation(100,100);
      this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    
      pane = this.getContentPane();
      pane.setLayout(new FlowLayout());  //NOTE!! THIS CAN BE CHANGED (see below)
-    
      CtoF = new JButton("CtoF");
      FtoC = new JButton("FtoC");
-     l = new JLabel("Temperature Converter!",null,JLabel.CENTER);
+     l = new JLabel("Temperature Converter!",null,JLabel.LEADING);
      t = new JTextField(12);
-     c = new JCheckBox("Overclock!!!");
+     u = new JTextField(12);
      pane.add(l);
      pane.add(CtoF);
+     CtoF.addActionListener(this);
      pane.add(FtoC);
+     FtoC.addActionListener(this);
      pane.add(t);
-     pane.add(c);
- }
+     pane.add(u);
+  }
   public static double CtoF(double c) {
     return c*1.8 + 32;
   }
     public void actionPerformed(ActionEvent e) {
-
+	try {
+	    if (e.getActionCommand().equals("FtoC")) {
+		u.setText(""+FtoC(Double.parseDouble(t.getText())));
+	    }
+	    else {
+		u.setText(""+CtoF(Double.parseDouble(t.getText())));
+	    }
+	}
+	catch (NumberFormatException n) {
+	    System.out.println("Please put a valid real number into the text box!");
+	}
     }
   public static double FtoC(double f) {
         return (5.0/9.0)*(f-32); 
